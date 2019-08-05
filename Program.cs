6ebdbb617026bb46ed8661cc66d2e5acca36512d
Ryan Baermann <rcbaermann@gmail.com>
2019-08-05 09:46:24 -0700

    finished csfundamentals1

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace fundamentals1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // print 1-255
+            for(int i = 1; i <= 255; i++){
+                Console.WriteLine(i);
+            }
+            
+            // print ints 1-100 that are divisible by 3 and 5, but not both
+            for(int i = 1; i <= 100; i++){
+                if(i % 3 == 0 && i % 5 == 0){
+                }
+                else if(i % 3 == 0 || i % 5 == 0){
+                    Console.WriteLine(i);
+                }
+            }
+
+            // print ints 1-100 that are divisible by 3 and 5, but as vars
+            for(int i = 1; i <= 100; i++){
+                if(i % 3 == 0 && i % 5 == 0){
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if(i % 5 == 0){
+                    Console.WriteLine("Buzz");
+                }
+                else if(i % 3 == 0){
+                    Console.WriteLine("Fizz");
+                }
+            }
+
+            // same as above, but as a while loop
+            int i = 1;
+            while(i <= 100){
+                if(i % 3 == 0 && i % 5 == 0){
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if(i % 5 == 0){
+                    Console.WriteLine("Buzz");
+                }
+                else if(i % 3 == 0){
+                    Console.WriteLine("Fizz");
+                }
+                i++;
+            }
+        }
+    }
+}
